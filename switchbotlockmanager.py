38b6bb1a267636bb21378e448ff397482ef60c80
Wajdi Bsoul <wbsoul@hotmail.com>
2022-12-07 11:08:02 +1100

    Added a periodic status update

--- switchbotlockmanager.py
+++ switchbotlockmanager.py
@@ -8,8 +8,10 @@ class SwitchBotLockManager(hass.Hass):
     self.log("Switchbot initialized in AppDaemon...")
     
     self.boolean_lock_deviceid = self.args["boolean_lock_deviceid"]
-    #self.boolean_lock_deviceid = 'input_boolean.lock_1'
     self.log("boolean_lock_deviceid= " + self.boolean_lock_deviceid)
+
+    self.status_update_interval_minutes = self.args["status_update_interval_minutes"]
+    self.log("status_update_interval_minutes= " + str(self.status_update_interval_minutes))
     
     self.switchbot = SwitchBot(token=self.args["api_token"], secret=self.args["api_secret"], nonce=str(uuid.uuid4()))
     #devices = self.switchbot.devices()
@@ -31,6 +33,8 @@ class SwitchBotLockManager(hass.Hass):
         self.call_service("notify/mobile_app_lya_l29", message="Initializing, Unknown lock_state: "+lockstatus["lock_state"])
     
     self.listen_state(self.state_change, self.boolean_lock_deviceid)
+
+    self.run_every(self.pull_status_update, "now", self.status_update_interval_minutes * 60)
   
   def state_change(self, entity, attribute, old, new, kwargs):
     self.log('old= ' + old)
@@ -59,3 +63,21 @@ class SwitchBotLockManager(hass.Hass):
       else:
         self.log("Nothing Done...")
         self.call_service("notify/mobile_app_lya_l29", message="Nothing done, Door is already: "+lockstatus["lock_state"])
+
+  def pull_status_update(self, kwargs):
+    self.log("Executing periodic update....")
+    self.switchbot = SwitchBot(token=self.args["api_token"], secret=self.args["api_secret"], nonce=str(uuid.uuid4()))
+    self.lock = self.switchbot.device(id=self.args["smartlock_deviceid"])
+    lockstatus = self.lock.status()
+    self.log("lock_state= " + lockstatus["lock_state"])
+    self.log("door_state= " + lockstatus["door_state"])
+    self.log("calibrate= " + str(lockstatus["calibrate"]))
+
+    if lockstatus["lock_state"] == "locked":
+      self.turn_on(self.boolean_lock_deviceid)
+    else:
+      if lockstatus["lock_state"] == "unlocked":
+        self.turn_off(self.boolean_lock_deviceid)
+      else:
+        self.log("Unknown lock_state: " +lockstatus["lock_state"])
+        self.call_service("notify/mobile_app_lya_l29", message="Periodic update, Unknown lock_state: "+lockstatus["lock_state"])
