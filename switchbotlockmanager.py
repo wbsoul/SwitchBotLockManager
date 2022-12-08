b22d4d1d014a81d183e025af6aa0ebc18eac92d1
Wajdi Bsoul <wbsoul@hotmail.com>
2022-12-08 11:31:14 +1100

    Logic and logs enhancments
    - removed extra status update while initializing
    - inlcuded the lock name in the push message sent
    - conditions for update status aree more tight now
    - made logging better

--- switchbotlockmanager.py
+++ switchbotlockmanager.py
@@ -5,6 +5,7 @@ from switchbot import SwitchBot
 class SwitchBotLockManager(hass.Hass):
 
   def initialize(self):
+    self.log('----------------------initialize')
     self.log("Switchbot initialized in AppDaemon...")
     
     self.boolean_lock_deviceid = self.args["boolean_lock_deviceid"]
@@ -13,33 +14,26 @@ class SwitchBotLockManager(hass.Hass):
     self.status_update_interval_minutes = self.args["status_update_interval_minutes"]
     self.log("status_update_interval_minutes= " + str(self.status_update_interval_minutes))
 
+    self.status_update_interval_seconds = self.status_update_interval_minutes * 60
+    self.log("status_update_interval_seconds= " + str(self.status_update_interval_seconds))
+
     self.push_notifiy_intity = self.args["push_notifiy_intity"]
     self.log("push_notifiy_intity= " + str(self.push_notifiy_intity))
     
-    self.switchbot = SwitchBot(token=self.args["api_token"], secret=self.args["api_secret"], nonce=str(uuid.uuid4()))
+    self.lock_name = self.boolean_lock_deviceid
+    self.lock_name = self.lock_name.split('.')[1]
+    self.log("lock_name= " + self.lock_name)
+
+    #self.switchbot = SwitchBot(token=self.args["api_token"], secret=self.args["api_secret"], nonce=str(uuid.uuid4()))
     #devices = self.switchbot.devices()
     #for device in devices:
     #  print(device)
-    self.lock = self.switchbot.device(id=self.args["smartlock_deviceid"])
-    lockstatus = self.lock.status()
-    self.log("lock_state= " + lockstatus["lock_state"])
-    self.log("door_state= " + lockstatus["door_state"])
-    self.log("calibrate= " + str(lockstatus["calibrate"]))
 
-    if lockstatus["lock_state"] == "locked":
-      self.turn_on(self.boolean_lock_deviceid)
-    else:
-      if lockstatus["lock_state"] == "unlocked":
-        self.turn_off(self.boolean_lock_deviceid)
-      else:
-        self.log("Unknown lock_state: " +lockstatus["lock_state"])
-        self.call_service(self.push_notifiy_intity, message="Initializing, Unknown lock_state: "+lockstatus["lock_state"])
-    
     self.listen_state(self.state_change, self.boolean_lock_deviceid)
-
-    self.run_every(self.pull_status_update, "now", self.status_update_interval_minutes * 60)
+    self.run_every(self.pull_status_update, "now", self.status_update_interval_seconds)
   
   def state_change(self, entity, attribute, old, new, kwargs):
+    self.log('----------------------state_change')
     self.log('old= ' + old)
     self.log('new= ' + new)
 
@@ -55,19 +49,20 @@ class SwitchBotLockManager(hass.Hass):
       self.lock.lock()
       self.turn_on(self.boolean_lock_deviceid )
       self.log("...Done Locking")
-      self.call_service(self.push_notifiy_intity, message="Unit door Locked")
+      self.call_service(self.push_notifiy_intity, message="Lock ["+self.lock_name+"] Locked")
     else:
       if new == "off" and lockstatus["lock_state"] == "locked":
         self.log("Attempting to UnLock...")
         self.lock.unlock()
         self.turn_off(self.boolean_lock_deviceid )
         self.log("...Done Unlocking")
-        self.call_service(self.push_notifiy_intity, message="Unit door UnLocked")
+        self.call_service(self.push_notifiy_intity, message="Lock ["+self.lock_name+"] UnLocked")
       else:
         self.log("Nothing Done...")
-        self.call_service(self.push_notifiy_intity, message="Nothing done, Door is already: "+lockstatus["lock_state"])
+        self.call_service(self.push_notifiy_intity, message="Nothing done, Lock ["+self.lock_name+"] is already: "+lockstatus["lock_state"] * 60)
 
   def pull_status_update(self, kwargs):
+    self.log('----------------------pull_status_update')
     self.log("Executing periodic update....")
     self.switchbot = SwitchBot(token=self.args["api_token"], secret=self.args["api_secret"], nonce=str(uuid.uuid4()))
     self.lock = self.switchbot.device(id=self.args["smartlock_deviceid"])
@@ -76,11 +71,19 @@ class SwitchBotLockManager(hass.Hass):
     self.log("door_state= " + lockstatus["door_state"])
     self.log("calibrate= " + str(lockstatus["calibrate"]))
 
-    if lockstatus["lock_state"] == "locked":
-      self.turn_on(self.boolean_lock_deviceid)
-    else:
-      if lockstatus["lock_state"] == "unlocked":
-        self.turn_off(self.boolean_lock_deviceid)
+    boolean_lock_device_state = self.get_state(self.boolean_lock_deviceid)
+    self.log("boolean_lock_device_state= " + boolean_lock_device_state)
+
+    if lockstatus["lock_state"] == "locked" or lockstatus["lock_state"] == "unlocked":
+      if lockstatus["lock_state"] == "locked" and boolean_lock_device_state == "off":
+        self.turn_on(self.boolean_lock_deviceid)
+        self.log("status updated to: " +lockstatus["lock_state"])
       else:
-        self.log("Unknown lock_state: " +lockstatus["lock_state"])
-        self.call_service(self.push_notifiy_intity, message="Periodic update, Unknown lock_state: "+lockstatus["lock_state"])
+        if lockstatus["lock_state"] == "unlocked"  and boolean_lock_device_state == "on":
+          self.turn_off(self.boolean_lock_deviceid)
+          self.log("status updated to: " +lockstatus["lock_state"])
+        else:
+          self.log("Nothing to update. lock_state: " +lockstatus["lock_state"])
+        
+    else:
+      self.call_service(self.push_notifiy_intity, message="Periodic update, Unknown lock_state: "+lockstatus["lock_state"])
